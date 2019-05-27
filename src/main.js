3dc32a0c0e2fa5057b401ea9b6910dfb60fa84c5
liangzhu <1678881243@qq.com>
2019-05-27 15:13:53 +0800

    去除vue的验证规则插件

--- src/main.js
+++ src/main.js
@@ -3,17 +3,14 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-// cookies插件
-import VueCookies from 'vue-cookies';
-Vue.use(VueCookies);
-// 表单验证
-var VueValidator = require("vue-validator");
-Vue.use(VueValidator);
-// iview插件
-import iView from 'iview';
+import iView from 'iview'; // iview插件
 import 'iview/dist/styles/iview.css';
-Vue.use(iView);
+import VueCookies from 'vue-cookies'; // cookies插件
 
+Vue.use(VueCookies);
+var VueValidator = require("vue-validator"); // 表单验证
+Vue.use(VueValidator);
+Vue.use(iView);
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
