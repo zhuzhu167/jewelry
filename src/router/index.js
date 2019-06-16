f1407165371ed95a763243dbdcb459b504128eb0
liangzhu <1678881243@qq.com>
2019-06-16 23:19:13 +0800

    懒加载

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,42 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import store from "@/vuex/store";
-import HelloWorld from '@/components/HelloWorld'
-import Header from '@/components/Header'
-import Index from '@/components/Index'
-import Login from '@/components/Login'
-import Register from '@/components/Register'
-import PersonInfo from '@/components/PersonInfo'
-import ShoppingCart from '@/components/ShoppingCart'
-import Order from '@/components/Order'
-import GoodList from '@/components/GoodList'
-import AddAss from '@/components/person/AddAss'
-import MdAss from '@/components/person/MdAss'
-import MdPwd from '@/components/person/MdPwd'
-import MdInfo from '@/components/person/MdInfo'
-import JewelryList from '@/components/JewelryList'
-import GoodInfo from '@/components/GoodInfo'
-import JewelryInfo from '@/components/JewelryInfo'
-import Pay from '@/components/Pay'
-import SelectConsignee from '@/components/SelectConsignee'
-import Code from '@/components/Code'
+const HelloWorld = resolve => require(['@/components/HelloWorld'], resolve);
+const Header = resolve => require(['@/components/Header'], resolve);
+const Index = resolve => require(['@/components/Index'], resolve);
+const Login = resolve => require(['@/components/Login'], resolve);
+const Register = resolve => require(['@/components/Register'], resolve);
+const PersonInfo = resolve => require(['@/components/PersonInfo'], resolve);
+const ShoppingCart = resolve => require(['@/components/ShoppingCart'], resolve);
+const Order = resolve => require(['@/components/Order'], resolve);
+const GoodList = resolve => require(['@/components/GoodList'], resolve);
+const AddAss = resolve => require(['@/components/person/AddAss'], resolve);
+const MdAss = resolve => require(['@/components/person/MdAss'], resolve);
+const MdPwd = resolve => require(['@/components/person/MdPwd'], resolve);
+const MdInfo = resolve => require(['@/components/person/MdInfo'], resolve);
+const JewelryList = resolve => require(['@/components/JewelryList'], resolve);
+const GoodInfo = resolve => require(['@/components/GoodInfo'], resolve);
+const JewelryInfo = resolve => require(['@/components/JewelryInfo'], resolve);
+const Pay = resolve => require(['@/components/Pay'], resolve);
+const SelectConsignee = resolve => require(['@/components/SelectConsignee'], resolve);
+const Code = resolve => require(['@/components/Code'], resolve);
+// import Index from '@/components/Index'
+// import Login from '@/components/Login'
+// import Register from '@/components/Register'
+// import PersonInfo from '@/components/PersonInfo'
+// import ShoppingCart from '@/components/ShoppingCart'
+// import Order from '@/components/Order'
+// import GoodList from '@/components/GoodList'
+// import AddAss from '@/components/person/AddAss'
+// import MdAss from '@/components/person/MdAss'
+// import MdPwd from '@/components/person/MdPwd'
+// import MdInfo from '@/components/person/MdInfo'
+// import JewelryList from '@/components/JewelryList'
+// import GoodInfo from '@/components/GoodInfo'
+// import JewelryInfo from '@/components/JewelryInfo'
+// import Pay from '@/components/Pay'
+// import SelectConsignee from '@/components/SelectConsignee'
+// import Code from '@/components/Code'
 Vue.use(Router)
 
 export default new Router({
